3160291ccc54ff95e72b59af50ccfadbb1d6b964
Cesar Gattano <cesar.gattano@gmail.com>
2025-05-07 12:11:12 +0200

    fix: :white_check_mark: Adaptation après retrait de EtreVivant.py
    Adaptation du code de Requin.py après fusion qui a amené EtreVivant.py

--- Requin.py
+++ Requin.py
@@ -6,8 +6,16 @@ class Requin(EtreVivant):
     def __init__(self, **kwargs):
         """Constructeur"""
         super().__init__(**kwargs)
-        self.energie = parametres.energie_requin
+        self.__energie = parametres.ENERGIE_INITIALE_REQUIN
 
+    @property
+    def energie(self) -> int:
+        return self.__energie
+
+    @energie.setter
+    def energie(self, nouvelle_energie) -> bool:
+        self.__energie = nouvelle_energie
+        return True
 
     def __repr__(self) -> str:
         """Affichage terminal
@@ -17,7 +25,6 @@ class Requin(EtreVivant):
         """
         attrs = ', '.join(f"{key}={value!r}" for key, value in vars(self).items())
         return f"{self.__class__.__name__}({attrs})"
-
 
     def s_alimenter(self, proie: Poisson) -> bool:
         """se déplace à la position de la proie et
@@ -30,18 +37,17 @@ class Requin(EtreVivant):
             bool: le poisson a-t-il été mangé ?
         """
         # Déplacement vers la proie
-        self.position = proie.position
+        self._position = proie.position
 
         # Mange la proie
-        self.energie += parametres.gain_energie_en_mangeant_poisson
-        proie.estVivant = False
+        self.energie += parametres.GAIN_ENERGIE_EN_MANGEANT_POISSON
+        proie._est_vivant = False
         return True
 
 
     def se_reproduire(self):
         # Creation d'un nouveau requin à la même position
-        nouveau_requin = Requin()
-        nouveau_requin.position = self.position.copy()
+        nouveau_requin = Requin(position=self.position)
         
         # Déplacement classique
         self.se_deplacer()
@@ -50,18 +56,17 @@ class Requin(EtreVivant):
 
     def mourir(self):
         if any([self.energie <= 0,
-               self.age > parametres.limite_age_requin]):
-            self.estVivant = False
+               self.age > parametres.LIMITE_AGE_REQUIN]):
+            self.est_vivant = False
 
+
+
+# Test conserver temporairement
 def test():
-    requin = Requin()
-    print(repr(requin))
-    requin.position = [1,1]
+    requin = Requin(position=(1,1))
     print(repr(requin))
 
-    poisson = Poisson()
-    print(repr(poisson))
-    poisson.position = [1,0]
+    poisson = Poisson(position=(1,0))
     print(repr(poisson))
     requin.s_alimenter(poisson)
     print(repr(requin))
@@ -74,7 +79,7 @@ def test():
     nouveau_requin.energie = 0
     nouveau_requin.mourir()
     print(repr(nouveau_requin))
-    requin.age = 100
+    requin._age = 100
     requin.mourir()
     print(repr(requin))
 
