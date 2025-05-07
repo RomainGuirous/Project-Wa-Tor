9cccfd5a6a2bc7ef4154d791535e0846b4c24afc
Romain Guirous <romaing_59@hotmail.fr>
2025-05-07 11:31:43 +0200

    perf: :art: Amelioration EtreVivant
    attributs passés en protected, grille adapté origine haut-gauche

--- EtreVivant.py
+++ EtreVivant.py
@@ -4,11 +4,11 @@ from parametres import NOMBRE_LIGNE_GRILLE, NOMBRE_COLONNE_GRILLE
 
 class EtreVivant:
 
-    est_vivant = True
+    _est_vivant = True
+    _age = 0
 
-    def __init__(self, position: tuple[int, int], age: int = 0) -> None:
+    def __init__(self, position: tuple[int, int]) -> None:
         self._position = position
-        self._age = age
 
     @property
     def position(self) -> tuple[int, int]:
@@ -22,15 +22,16 @@ class EtreVivant:
         direction = directions_possibles[randint(0, 3)]  # une direction au hasard
 
         # pour chaque modification de la position, on vérifie qu'on reste dans la grille (avec modulo colonne ligne)
+        # prend en compte que l'origine de la grille en haut à gauche
         if direction == "N":
             self._position = (
                 self._position[0],
-                (self._position[1] + 1) % NOMBRE_LIGNE_GRILLE,
+                (self._position[1] - 1) % NOMBRE_LIGNE_GRILLE,
             )
         if direction == "S":
             self._position = (
                 self._position[0],
-                (self._position[1] - 1) % NOMBRE_LIGNE_GRILLE,
+                (self._position[1] + 1) % NOMBRE_LIGNE_GRILLE,
             )
         if direction == "O":
             self._position = (
